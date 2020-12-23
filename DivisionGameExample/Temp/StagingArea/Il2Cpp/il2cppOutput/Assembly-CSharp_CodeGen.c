b424d8587d4dc25c623cf38952a077e7888623af
Fırat <drsn1995@gmail.com>
2020-12-23 05:30:44 +0300

    DivisionGame

---
+++ DivisionGameExample/Temp/StagingArea/Il2Cpp/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,179 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void DivisionGameExample.GameManager::Start()
+extern void GameManager_Start_m489135413E5AFD65D314A692F9646659CE4F1216 (void);
+// 0x00000002 System.Void DivisionGameExample.GameManager::SquareCreate()
+extern void GameManager_SquareCreate_m026F924CD6D03A51E2BCD5F28EFB406C557B8A4E (void);
+// 0x00000003 System.Collections.IEnumerator DivisionGameExample.GameManager::SquareRoutine()
+extern void GameManager_SquareRoutine_m2DAEAD647694EEACFD500CE602EB1FF483B5D033 (void);
+// 0x00000004 System.Void DivisionGameExample.GameManager::SquareNumberFill()
+extern void GameManager_SquareNumberFill_m45A91A9D3E767CC004D56047552FDB5E322F9F62 (void);
+// 0x00000005 System.Void DivisionGameExample.GameManager::ClickAnswer()
+extern void GameManager_ClickAnswer_mC0C6E5526DA6D8BFEDA74FF2197D8053302964BA (void);
+// 0x00000006 System.Void DivisionGameExample.GameManager::ResultPanel()
+extern void GameManager_ResultPanel_mBF92C97B7EB9956AC5D7A1964B86E02B6C5A7687 (void);
+// 0x00000007 System.Void DivisionGameExample.GameManager::QuestionPanelScale()
+extern void GameManager_QuestionPanelScale_m5F47A3895FC94C4A57B97C7A556281F4F8CF726B (void);
+// 0x00000008 System.Void DivisionGameExample.GameManager::QuestionCreate()
+extern void GameManager_QuestionCreate_mE11EE66F402175549030C5C5D753001651AFA8AE (void);
+// 0x00000009 System.Void DivisionGameExample.GameManager::LifeCreate()
+extern void GameManager_LifeCreate_m04B910339CD647C16FFF6BBA65647D8C53AF64F8 (void);
+// 0x0000000A System.Void DivisionGameExample.GameManager::.ctor()
+extern void GameManager__ctor_mB83D65DAB54B6F60725622D3EE8A787DB203391B (void);
+// 0x0000000B System.Void DivisionGameExample.GameManager::<SquareCreate>b__22_0()
+extern void GameManager_U3CSquareCreateU3Eb__22_0_m2965FF9C3AB1F5906DF79EE39B322B4C529C678F (void);
+// 0x0000000C System.Void DivisionGameExample.LifeManager::killLife()
+extern void LifeManager_killLife_m69ABDF05E8F2F52AA7014132BE2CEE249EF2DD35 (void);
+// 0x0000000D System.Void DivisionGameExample.LifeManager::.ctor()
+extern void LifeManager__ctor_m0F917F5D78D146B9E801F68A606985749281DCC0 (void);
+// 0x0000000E System.Void DivisionGameExample.PanelManager::Start()
+extern void PanelManager_Start_mE5F0D6A1D9D5D88F4CDA934081C80073AB56FFC3 (void);
+// 0x0000000F System.Void DivisionGameExample.PanelManager::.ctor()
+extern void PanelManager__ctor_m99496CDAC74841C18C423E8190EEB53D2A877879 (void);
+// 0x00000010 System.Void DivisionGameExample.ResultManager::RestartGame()
+extern void ResultManager_RestartGame_m4B2C6CCD5243A2AB3744C4CF74F0064A7031052B (void);
+// 0x00000011 System.Void DivisionGameExample.ResultManager::HomeScene()
+extern void ResultManager_HomeScene_mBDD5E548C9BE595A98A2C0FABF769AF66426A581 (void);
+// 0x00000012 System.Void DivisionGameExample.ResultManager::.ctor()
+extern void ResultManager__ctor_m370E5E70C8933F5314AC8CB03AB4A5228D1B2B26 (void);
+// 0x00000013 System.Void DivisionGameExample.ScoreManager::Start()
+extern void ScoreManager_Start_mBFBA59838FA87ACA834539055CC0148B3AF4B187 (void);
+// 0x00000014 System.Void DivisionGameExample.ScoreManager::CalculateScore(System.String)
+extern void ScoreManager_CalculateScore_m753B482BBE0841C05D3F2208C4BCBDCA70173EFB (void);
+// 0x00000015 System.Int32 DivisionGameExample.ScoreManager::GetScore()
+extern void ScoreManager_GetScore_mF21C20375A648D34B00F61C986DB4D3E64B8B2BC (void);
+// 0x00000016 System.Void DivisionGameExample.ScoreManager::.ctor()
+extern void ScoreManager__ctor_m992AD3D104CE9E76A1FC92144041855B59F05FB5 (void);
+// 0x00000017 System.Void DivisionGameExample.MenuManager::Start()
+extern void MenuManager_Start_mBB5F495466E7A7695D1935871DC275E4A6C73341 (void);
+// 0x00000018 System.Void DivisionGameExample.MenuManager::FadeIn()
+extern void MenuManager_FadeIn_m7400CC104A1B20AB1479814F1BD97297B0B02562 (void);
+// 0x00000019 System.Void DivisionGameExample.MenuManager::ExitApplication()
+extern void MenuManager_ExitApplication_m220839C38A5BBF5039111AB6810838AC6E2B8534 (void);
+// 0x0000001A System.Void DivisionGameExample.MenuManager::StartGame()
+extern void MenuManager_StartGame_m88444D74F6AA2257ABB6E753EDD274C8861E7E4C (void);
+// 0x0000001B System.Void DivisionGameExample.MenuManager::.ctor()
+extern void MenuManager__ctor_m3F7CD696927F83F52F641260D18FAB32BDBA4FC2 (void);
+// 0x0000001C System.Void DivisionGameExample.GameManager_<SquareRoutine>d__23::.ctor(System.Int32)
+extern void U3CSquareRoutineU3Ed__23__ctor_mE37784433921078F7FB14B5A5497A68ACC30652D (void);
+// 0x0000001D System.Void DivisionGameExample.GameManager_<SquareRoutine>d__23::System.IDisposable.Dispose()
+extern void U3CSquareRoutineU3Ed__23_System_IDisposable_Dispose_mCEEC6B991D0383C947E4654AD7D8FF648D84D01F (void);
+// 0x0000001E System.Boolean DivisionGameExample.GameManager_<SquareRoutine>d__23::MoveNext()
+extern void U3CSquareRoutineU3Ed__23_MoveNext_mBC8EA152F2ADE32A5E759FBF1D41DF488D24492D (void);
+// 0x0000001F System.Void DivisionGameExample.GameManager_<SquareRoutine>d__23::<>m__Finally1()
+extern void U3CSquareRoutineU3Ed__23_U3CU3Em__Finally1_m441E372B81ED35E74A3EE16D361887C462FD1969 (void);
+// 0x00000020 System.Object DivisionGameExample.GameManager_<SquareRoutine>d__23::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CSquareRoutineU3Ed__23_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA78D779516C99BE4A2FA2B52AA422CD410689C61 (void);
+// 0x00000021 System.Void DivisionGameExample.GameManager_<SquareRoutine>d__23::System.Collections.IEnumerator.Reset()
+extern void U3CSquareRoutineU3Ed__23_System_Collections_IEnumerator_Reset_mCE9623D27936083A28A8D7F29412397F6E4D5475 (void);
+// 0x00000022 System.Object DivisionGameExample.GameManager_<SquareRoutine>d__23::System.Collections.IEnumerator.get_Current()
+extern void U3CSquareRoutineU3Ed__23_System_Collections_IEnumerator_get_Current_m56D72B015C4626DDE9B48FCB7758F34376E96657 (void);
+static Il2CppMethodPointer s_methodPointers[34] = 
+{
+	GameManager_Start_m489135413E5AFD65D314A692F9646659CE4F1216,
+	GameManager_SquareCreate_m026F924CD6D03A51E2BCD5F28EFB406C557B8A4E,
+	GameManager_SquareRoutine_m2DAEAD647694EEACFD500CE602EB1FF483B5D033,
+	GameManager_SquareNumberFill_m45A91A9D3E767CC004D56047552FDB5E322F9F62,
+	GameManager_ClickAnswer_mC0C6E5526DA6D8BFEDA74FF2197D8053302964BA,
+	GameManager_ResultPanel_mBF92C97B7EB9956AC5D7A1964B86E02B6C5A7687,
+	GameManager_QuestionPanelScale_m5F47A3895FC94C4A57B97C7A556281F4F8CF726B,
+	GameManager_QuestionCreate_mE11EE66F402175549030C5C5D753001651AFA8AE,
+	GameManager_LifeCreate_m04B910339CD647C16FFF6BBA65647D8C53AF64F8,
+	GameManager__ctor_mB83D65DAB54B6F60725622D3EE8A787DB203391B,
+	GameManager_U3CSquareCreateU3Eb__22_0_m2965FF9C3AB1F5906DF79EE39B322B4C529C678F,
+	LifeManager_killLife_m69ABDF05E8F2F52AA7014132BE2CEE249EF2DD35,
+	LifeManager__ctor_m0F917F5D78D146B9E801F68A606985749281DCC0,
+	PanelManager_Start_mE5F0D6A1D9D5D88F4CDA934081C80073AB56FFC3,
+	PanelManager__ctor_m99496CDAC74841C18C423E8190EEB53D2A877879,
+	ResultManager_RestartGame_m4B2C6CCD5243A2AB3744C4CF74F0064A7031052B,
+	ResultManager_HomeScene_mBDD5E548C9BE595A98A2C0FABF769AF66426A581,
+	ResultManager__ctor_m370E5E70C8933F5314AC8CB03AB4A5228D1B2B26,
+	ScoreManager_Start_mBFBA59838FA87ACA834539055CC0148B3AF4B187,
+	ScoreManager_CalculateScore_m753B482BBE0841C05D3F2208C4BCBDCA70173EFB,
+	ScoreManager_GetScore_mF21C20375A648D34B00F61C986DB4D3E64B8B2BC,
+	ScoreManager__ctor_m992AD3D104CE9E76A1FC92144041855B59F05FB5,
+	MenuManager_Start_mBB5F495466E7A7695D1935871DC275E4A6C73341,
+	MenuManager_FadeIn_m7400CC104A1B20AB1479814F1BD97297B0B02562,
+	MenuManager_ExitApplication_m220839C38A5BBF5039111AB6810838AC6E2B8534,
+	MenuManager_StartGame_m88444D74F6AA2257ABB6E753EDD274C8861E7E4C,
+	MenuManager__ctor_m3F7CD696927F83F52F641260D18FAB32BDBA4FC2,
+	U3CSquareRoutineU3Ed__23__ctor_mE37784433921078F7FB14B5A5497A68ACC30652D,
+	U3CSquareRoutineU3Ed__23_System_IDisposable_Dispose_mCEEC6B991D0383C947E4654AD7D8FF648D84D01F,
+	U3CSquareRoutineU3Ed__23_MoveNext_mBC8EA152F2ADE32A5E759FBF1D41DF488D24492D,
+	U3CSquareRoutineU3Ed__23_U3CU3Em__Finally1_m441E372B81ED35E74A3EE16D361887C462FD1969,
+	U3CSquareRoutineU3Ed__23_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA78D779516C99BE4A2FA2B52AA422CD410689C61,
+	U3CSquareRoutineU3Ed__23_System_Collections_IEnumerator_Reset_mCE9623D27936083A28A8D7F29412397F6E4D5475,
+	U3CSquareRoutineU3Ed__23_System_Collections_IEnumerator_get_Current_m56D72B015C4626DDE9B48FCB7758F34376E96657,
+};
+static const int32_t s_InvokerIndices[34] = 
+{
+	23,
+	23,
+	14,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	26,
+	10,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	32,
+	23,
+	102,
+	23,
+	14,
+	23,
+	14,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	34,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
