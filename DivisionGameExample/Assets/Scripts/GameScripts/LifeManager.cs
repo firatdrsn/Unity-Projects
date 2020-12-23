674481720777507affbfac773008781cd30889cf
Fırat <drsn1995@gmail.com>
2020-12-23 06:43:49 +0300

    Division Game

---
+++ DivisionGameExample/Assets/Scripts/GameScripts/LifeManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DivisionGameExample
+{
+    public class LifeManager : MonoBehaviour
+    {
+
+        public void killLife()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ DivisionGameExample/Assets/Scripts/GameScripts/PanelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+namespace DivisionGameExample
+{
+    public class PanelManager : MonoBehaviour
+    {
+
+        void Start()
+        {
+            gameObject.GetComponent<CanvasGroup>().DOFade(0, 1.5f);
+        }
+
+    }
+}
